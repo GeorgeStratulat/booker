4dc5c94fa3b310af486c74877d5f271ad00113e4
George <george.stratulat10@gmail.com>
2023-06-19 00:27:48 +0300

    pana la etapa 5

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
-const express = require('express')
+const express = require('express');
+const fs = require("fs");
 const path = require("path");
+const sharp=require('sharp');
 const app = express()
 const port = 8080
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
+console.log("Directorul curent:",__dirname);
+console.log("Fisierul curent:",__filename);
+
 app.use("/resources", express.static(__dirname + "/resources"))
 
 const erori = require("./erori.json");
 
-const folders = ["temp"];
+const foldere =["temp", "temp1", "backup", "poze_uploadate"];
+
+const imagini = () => {
+  const json = fs.readFileSync(__dirname + "/resources/images/galerie/imagini_galerie.json");
+  const imaginiJSON = JSON.parse(json);
+  let imagini = imaginiJSON.imagini;
+  const caleImagini = path.join(__dirname, imaginiJSON.cale_galerie)
+  for (let img of imagini) {
+      [numeFisier, ext] = img.cale_fisier.split(".")
+      let caleFisier = path.join(caleImagini, img.cale_fisier)
+      let caleFisierTemp = path.join(caleImagini, numeFisier + "_temp.jpg")
+      sharp(caleFisier).resize({height: 300, width: 300}).toFile(caleFisierTemp)
+      img.fisier = path.join("/", imaginiJSON.cale_galerie, numeFisier + "_temp.jpg")
+  }
+  return imagini
+}
+
+globals = {
+  imagini_galerie: imagini()
+}
+
+for (let folder of foldere){
+    let cale=path.join(__dirname, folder);
+    if (!fs.existsSync(cale)){
+        fs.mkdirSync(cale);
+    }
+}
 
 app.get(["/", "/index", "/home"], (req, res) => {
     const ipAddress = req.socket.remoteAddress;
-    console.log(req.socket)
     res.render('pagini/index', { ipAddress });
 });
 
